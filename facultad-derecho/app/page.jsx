1b5e30e9909e5f8bbb64552fb04e32d59efaa4d9
FabioSierrare <fabiosierra281@gmail.com>
2025-08-26 11:45:14 -0500

    Update page.jsx

--- facultad-derecho/app/page.jsx
+++ facultad-derecho/app/page.jsx
@@ -3,7 +3,6 @@ import { jwtDecode } from "jwt-decode";
 import { useRouter } from "next/navigation"; 
 import { useState } from 'react';
 import { postData } from '@/components/FetchPost';
-import { type } from "os";
 import { useAuth } from "@/components/AuthCont";
 
 export const runtime = "edge"
